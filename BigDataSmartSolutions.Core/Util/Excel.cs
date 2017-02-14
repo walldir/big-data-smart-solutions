7689d61eaaf05afbc56ff5356f26d96b91085e4e
Waldir de Paula <walldir@gmail.com>
2017-02-14 20:08:22 -0300

    Projeto de Upload

---
+++ BigDataSmartSolutions.Core/Util/Excel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Web;
+using Excel;
+
+namespace BigDataSmartSolutions.Core.Util
+{
+    public static class Excel
+    {
+        public static bool IsValido(HttpPostedFileBase file)
+        {
+            if(file.ContentLength < 0)
+                throw new Exception("Arquivo corrompido tente novamente.");
+            
+            if(file.ContentType != "application/octet-stream")
+                throw new Exception("Arquivo inválido.");
+
+            var extension = Path.GetExtension(file.FileName);
+            if (extension == null || (extension.ToLower() != ".xls" && extension.ToLower() != ".xlsx"))
+                throw new Exception("Formato inválido");
+
+            return true;
+        }
+
+        public static string ExcelToCsv(string path, HttpPostedFileBase file)
+        {
+            if (file.FileName != null)
+            {
+                var filePath = Path.Combine(path, Path.GetFileName(file.FileName));
+
+                file.SaveAs(filePath);
+                
+                FileStream stream = new FileStream(filePath, FileMode.Open);
+                IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+
+                DataSet result = excelReader.AsDataSet();
+
+                return DataTableToCsv(result.Tables[0]);
+            }
+
+            return "";
+        }
+
+        private static string DataTableToCsv(DataTable dataTable)
+        {
+            StringBuilder sb = new StringBuilder();
+            var sep = ";";
+            var qtdColumns = dataTable.Columns.Count;
+
+            for (int x = 0; x < qtdColumns; x++)
+            {
+                sb.Append(dataTable.Columns[x]);
+
+                if(x < qtdColumns - 1)
+                    sb.Append(sep);
+            }
+
+            sb.AppendLine();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                for (int x = 0; x < qtdColumns; x++)
+                {
+                    sb.Append(row[x]);
+
+                    if (x < qtdColumns - 1)
+                        sb.Append(sep);
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+    }
+}
--- BigDataSmartSolutions.Web/Controllers/HomeController.cs
+++ BigDataSmartSolutions.Web/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +10,37 @@ namespace BigDataSmartSolutions.Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public FileContentResult ConvertExcelToCsv()
+        {
+            if (Request.Files.Count > 0)
+            {
+                HttpPostedFileBase file = Request.Files[0];
+                try
+                {
+                    if (file != null && Core.Util.Excel.IsValido(file))
+                    {
+                        var csv = Core.Util.Excel.ExcelToCsv(Server.MapPath("~/Content"), file);
+
+                        return File(new System.Text.UTF8Encoding().GetBytes(csv), "text/csv",
+                            $"{Path.GetFileNameWithoutExtension(file.FileName)}.csv");
+                    }
+                    else
+                    {
+                        throw new Exception("Erro ao carregar arquivo");
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+            else
+            {
+                throw new Exception("Nenhum arquivo selecionado");
+            }
         }
     }
 }
